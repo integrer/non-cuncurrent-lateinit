adf4b95a41443ca856d4375135534c3c2f75150e
Nikita Bibik <osbib2@gmail.com>
2019-11-24 21:12:59 +0300

    Implement late initialization with ref structures (std::cell).

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,34 @@
 use std::option::Option;
+use std::ops::Deref;
+use std::cell::{RefCell, Ref, RefMut};
+use std::rc::Rc;
 
 pub struct Lateinit<'a, T> {
-    val: Option<T>,
+    val: Rc<RefCell<Option<T>>>,
     init: &'a dyn Fn() -> T,
 }
 
-impl Lateinit<_> {
-    pub fn new<T>(initializer: &dyn Fn() -> T) -> Self<T> {
-        Self {val: None, init: initializer}
+impl<'a, T> Lateinit<'a, T> {
+    pub fn new(initializer: &'a dyn Fn() -> T) -> Lateinit<T> {
+        Self {val: Rc::new(RefCell::new(None)), init: initializer}
     }
-}
 
-impl<T> Lateinit<T> {
-    pub fn unwrap(&mut self) -> T {
-        self.val.get_or_insert(&self.init())
+    fn init(&self, ref_mut: &mut RefMut<Option<T>>) {
+        if ref_mut.is_none() {
+            **ref_mut = Some((self.init)())
+        }
+    }
+
+    fn borrow(&self) -> Ref<T> {
+        {
+            self.init(&mut self.val.borrow_mut());
+        }
+        Ref::map(self.val.borrow(), |v| v.as_ref().unwrap())
+    }
+
+    fn borrow_mut(&mut self) -> RefMut<T> {
+        let mut r = self.val.borrow_mut();
+        self.init(&mut r);
+        RefMut::map(r, |v| v.as_mut().unwrap())
     }
 }
